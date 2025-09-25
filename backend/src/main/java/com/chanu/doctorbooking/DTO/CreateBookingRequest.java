c03881bc9b36862661e64c99eee46b56bf140f2f
arrapriyanka <arrapriyanka2@gmail.com>
2025-09-25 18:23:01 +0530

    ceratebookingrequest code added

---
+++ backend/src/main/java/com/chanu/doctorbooking/DTO/CreateBookingRequest.java
@@ -0,0 +1,8 @@
+package com.chanu.doctorbooking.dto;
+
+public class CreateBookingRequest {
+  public Long slotId;
+  public String patientName;
+  public String patientEmail;
+  public String patientPhone;
+}
