b76efc1f6bfcbc691e3b7ddf5cf4ac2d26e5a54c
arrapriyanka <arrapriyanka2@gmail.com>
2025-09-25 22:02:42 +0530

    Updated webpackage.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,21 +5,33 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
+      {
+        test: /\.(js|jsx)$/,   // process both .js and .jsx
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
     ]
   },
-  resolve: { extensions: ['.js', '.jsx'] },
-  plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+  },
 };
