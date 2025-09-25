c9d3d1531b560cff7cfc79a9a0d9aafc0ce4756c
arrapriyanka <arrapriyanka2@gmail.com>
2025-09-25 23:41:49 +0530

    App.jsx code changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import DoctorList from "./DoctorList";
 import MyBookings from "./MyBookings";
+import BookingUI from "./components/BookingUI"; // ✅ new UI
 
 function App() {
   return (
     <div>
       <h1>Doctor Booking App</h1>
+
+      {/* Navigation */}
       <nav>
-        <Link to="/">Doctors</Link> | <Link to="/bookings">My Bookings</Link>
+        <Link to="/">Doctors</Link> |{" "}
+        <Link to="/bookings">My Bookings</Link> |{" "}
+        <Link to="/booking-ui">Booking UI</Link>
       </nav>
+
+      {/* Routes */}
       <Routes>
         <Route path="/" element={<DoctorList />} />
         <Route path="/bookings" element={<MyBookings />} />
+        <Route path="/booking-ui" element={<BookingUI />} /> {/* ✅ new route */}
       </Routes>
     </div>
   );
