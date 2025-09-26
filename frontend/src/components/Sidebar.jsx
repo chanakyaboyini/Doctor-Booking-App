aceec91ae96b8587a75159e206f4133c0261e8bd
arrapriyanka <arrapriyanka2@gmail.com>
2025-09-26 10:31:03 +0530

    code changed for sidebar.jsx

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Boctorre</h2>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>My Appointments</li>
+          <li>Doctors</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
